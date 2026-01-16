818e94c4c24034bf23c0500125d89ed161772035
Yoonho Cha <0119hoya@naver.com>
2026-01-16 23:46:58 +0900

    feat: 이력관리 엔티티 추가

---
+++ src/main/java/com/example/meetupflow/domain/status/UserRole.java
@@ -0,0 +1,13 @@
+package com.example.meetupflow.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    USER("일반 사용자"),
+    ADMIN("관리자");
+
+    private final String description;
+}
---
+++ src/main/java/com/example/meetupflow/domain/status/UserStatus.java
@@ -0,0 +1,15 @@
+package com.example.meetupflow.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserStatus {
+    NORMAL("정상"),
+    WITHDRAWAL("탈퇴"),
+    DORMANT("휴면");
+
+    // 상태에 대한 한글 설명을 저장하기 위한 필드
+    private final String description;
+}

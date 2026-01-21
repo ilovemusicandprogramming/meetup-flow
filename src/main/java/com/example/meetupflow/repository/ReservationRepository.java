513bc5960e6c7731d50e3562d1f7f7651d0bb08c
Yoonho Cha <0119hoya@naver.com>
2026-01-21 18:05:17 +0900

    fix: 불필요한 import 삭제

--- src/main/java/com/example/meetupflow/repository/ReservationRepository.java
+++ src/main/java/com/example/meetupflow/repository/ReservationRepository.java
@@ -4,11 +4,14 @@ import com.example.meetupflow.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>, ReservationRepositoryCustom {
 
+    /*
+     * TODO: 리팩토링 필요
+     * Querydsl로 전환 예정
+     */
     @Query("select r from Reservation r join fetch r.user join fetch r.meetingRoom")
     List<Reservation> findAllWithUserAndRoom();
 }
